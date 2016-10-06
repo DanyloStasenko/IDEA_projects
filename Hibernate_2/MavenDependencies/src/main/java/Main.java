aa4056799da5e62e806b9a5cfdd529ca103d3002
Danylo Stasenko <danylo.stasenko@gmail.com>
2016-10-06 11:48:35 +0300

    Lesson 2 - "xml mapping"

---
+++ Hibernate_2/MavenDependencies/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.hibernate.SessionFactory;
+
+/**
+ * Created by Admin on 06.10.2016.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SessionFactory sessionFactory = HibernateUtil.getSessionfactory();
+    }
+}
---
+++ Hibernate_2/MavenDependencies/src/main/java/User.java
@@ -0,0 +1,60 @@
+import java.io.Serializable;
+
+public class User implements Serializable
+{
+    private  long id;
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public User()
+    {
+
+    }
+
+    public User(long id)
+    {
+        this.id = id;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+}
