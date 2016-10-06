bb6926e13fa3bf967e66a9a72b62e2b86fd9ed84
Danylo Stasenko <danylo.stasenko@gmail.com>
2016-10-06 10:28:29 +0300

    lesson_1

---
+++ Hibernate_1/MavenDependencies/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.hibernate.SessionFactory;
+
+/**
+ * Created by Admin on 06.10.2016.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SessionFactory sessionFactory = HibernateUtil.getSessionfactory();
+    }
+
+}
