e53837a7b866d4b128e700bd6aa0ef2b5739d6b1
Danylo Stasenko <danylo.stasenko@gmail.com>
2016-11-13 18:36:27 +0200

    JSP: Lesson 10: "Sessions"

---
+++ jsp_10/jsp/src/jsp/HomeServlet.java
@@ -0,0 +1,58 @@
+package jsp;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class HomeServlet extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+
+	private Integer status;
+	
+	public HomeServlet(){}
+
+	
+	@Override
+	public void init() throws ServletException {
+		status = Integer.parseInt(getServletConfig().getInitParameter("paramname"));
+		System.out.println("Status: " +status);
+	}
+
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("key", "value");
+		
+		// session.removeAttribute("key");
+		// session.getAttribute("key"); // returns Object
+		// session.setMaxInactiveInterval(5); // interval
+		
+		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/home.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// get data
+		int a = Integer.parseInt(request.getParameter("a"));
+		int b = Integer.parseInt(request.getParameter("b"));
+		
+		// do logic
+		int result = a + b;
+		
+		// send data
+		request.setAttribute("result", result);
+		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/post.jsp");
+		requestDispatcher.forward(request, response);
+	}
+}
