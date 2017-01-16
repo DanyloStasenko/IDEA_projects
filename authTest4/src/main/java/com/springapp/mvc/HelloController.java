473eca19fc06a39ec457a68aa6abe2efe32adb71
DanyloStasenko <danylo.stasenko@gmail.com>
2017-01-16 22:47:24 +0200

    Spring Security test created

---
+++ authTest4/src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,46 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    public ModelAndView welcomePage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is welcome page!");
+        model.setViewName("hello");
+        return model;
+
+    }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+     public ModelAndView adminPage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("admin");
+
+        return model;
+
+    }
+
+    @RequestMapping(value = "/user**", method = RequestMethod.GET)
+    public ModelAndView userPage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("user");
+
+        return model;
+
+    }
+
+}
