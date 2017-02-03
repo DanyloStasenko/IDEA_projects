59102ce3286203567c2748169e588e71464408dd
DanyloStasenko <danylo.stasenko@gmail.com>
2017-02-03 23:12:16 +0200

    Lesson 05 - "Prototype"

---
+++ BasePatterns/src/prototype/ProjectFactory.java
@@ -0,0 +1,18 @@
+package prototype;
+
+public class ProjectFactory {
+    Project project;
+
+    public ProjectFactory(Project project) {
+        this.project = project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    Project cloneProject(){
+        return (Project) project.copy();
+    }
+
+}
