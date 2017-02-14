a2789fd3db9228a122796e3c8b8cfcb2b4b7535d
DanyloStasenko <danylo.stasenko@gmail.com>
2017-02-14 21:38:26 +0200

    Lesson 13 - "Chains of responsibility" added

---
+++ BasePatterns/src/behavioral/chain/SimpleReportNotifier.java
@@ -0,0 +1,12 @@
+package behavioral.chain;
+
+public class SimpleReportNotifier extends Notifier{
+    public SimpleReportNotifier(int priority) {
+        super(priority);
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("Notifying using simple report: " + message);
+    }
+}
