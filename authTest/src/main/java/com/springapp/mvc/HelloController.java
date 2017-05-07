641004f0a9561cda8df63ce249722aa1c0e2be39
DanyloStasenko <danylo.stasenko@gmail.com>
2017-05-07 10:14:50 +0300

    auth test created

---
+++ authTest/src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,48 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String welcomeAdmin(ModelMap model) {
+
+        model.addAttribute("message", "Spring Security - ROLE_ADMIN");
+        return "hello";
+
+    }
+
+}
+
+
+/*
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    public ModelAndView welcomePage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is welcome page!");
+        model.setViewName("hello");
+        return model;
+    }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("admin");
+
+        return model;
+    }
+}*/
---
+++ authTest/src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,19 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<body>
+<div class="ads-in-post hide_if_width_less_800">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- new 728x90 - After2ndH4 -->
+    <ins class="adsbygoogle hide_if_width_less_800"
+         style="display:inline-block;width:728px;height:90px"
+         data-ad-client="ca-pub-2836379775501347"
+         data-ad-slot="3642936086"
+         data-ad-region="mkyongregion"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div><h2>Message : ${message}</h2>
+
+<%--<a href="<c:url value="j_spring_security_logout" />" > Logout</a>--%>
+</body>
+</html>
