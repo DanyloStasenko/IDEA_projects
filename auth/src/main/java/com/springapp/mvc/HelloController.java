cdb4b879266c6e0d9efbe360c7e8d79a9349f6a3
DanyloStasenko <danylo.stasenko@gmail.com>
2017-06-04 23:36:57 +0300

    auth created

---
+++ auth/src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,18 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//@RequestMapping("/editPresPage.do")
+//@RequestMapping("/")
+@Controller
+@RequestMapping("/editPresPage.do")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+}
