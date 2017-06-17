86349bfdb80024e6620891493b7463274416e592
DanyloStasenko <danylo.stasenko@gmail.com>
2017-06-17 13:27:06 +0300

    authTest2 created

---
+++ authTest2/src/main/java/com/springapp/mvc/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.springapp.mvc.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
